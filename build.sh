49cf4582edbb88b66c4972c307b6a8eee7c7c848
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2020-11-10 19:31:12 -0300

    Adding build and publish script.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_USER $REPOSITORY_URL --password-stdin 
+
+docker build -t $REPOSITORY_URL/$REPOSITORY_NAME/base-image:latest .
+docker build -t $REPOSITORY_URL/$REPOSITORY_NAME/base-image:1.0.0 .
---
+++ publish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker push $REPOSITORY_URL/$REPOSITORY_NAME/base-image:latest
+docker push $REPOSITORY_URL/$REPOSITORY_NAME/base-image:1.0.0
