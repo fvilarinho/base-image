c81c3834bb72a30675339df9dee62c5647f9edf1
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2020-11-16 22:12:07 -0300

    Code improvements.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_USER $REPOSITORY_URL --password-stdin 
+if [ ! -z "$REPOSITORY_URL" ]; then 
+	echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_USER $REPOSITORY_URL --password-stdin
+fi 
+
+export BUILD_VERSION=1.0.0
+
+docker-compose build
+
+export BUILD_VERSION=latest
 
 docker-compose build
--- publish.sh
+++ publish.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+export BUILD_VERSION=1.0.0
+
+docker-compose push
+
+export BUILD_VERSION=latest
+
 docker-compose push
