e0f74822f18813091be394bd269aa592af8449c8
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2020-11-16 21:55:35 -0300

    Update build and publish.

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_USER $REPOSITORY_URL --password-stdin 
 
-docker build -t $REPOSITORY_URL/$REPOSITORY_NAME/base-image:latest .
-docker build -t $REPOSITORY_URL/$REPOSITORY_NAME/base-image:1.0.0 .
+docker-compose build
--- publish.sh
+++ publish.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker push $REPOSITORY_URL/$REPOSITORY_NAME/base-image:latest
-docker push $REPOSITORY_URL/$REPOSITORY_NAME/base-image:1.0.0
+docker-compose push
