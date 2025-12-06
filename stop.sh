6af39f54fac7b32917f0bac5f039a42e34c6a26c
Felipe Vilarinho <fvilarinho@gmail.com>
2025-12-06 19:08:23 -0300

    Code improvements.

--- stop.sh
+++ stop.sh
@@ -4,8 +4,4 @@ if [ -f .env ]; then
   source .env
 fi
 
-if [ ! -z "$DOCKER_REGISTRY_URL" ]; then
-	echo $DOCKER_REGISTRY_PASSWORD | docker login -u $DOCKER_REGISTRY_ID $DOCKER_REGISTRY_URL --password-stdin
-fi
-
 docker compose down
